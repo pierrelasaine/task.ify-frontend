531f7d01a964ac9dfcdb64bc537d0282b1b8c556
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-08-07 16:13:35 -0700

    Added Session Check to Protected Routes

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,31 @@
-import ProtectedRouteProps from '../../../interfaces/ProtectedRouteProps'   
+import ProtectedRouteProps from '../../../interfaces/ProtectedRouteProps'
 import { Navigate } from 'react-router-dom'
 import Loading from '../Loading/Loading'
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element, appState, isLoading, fallback }) => {
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+    element,
+    appState,
+    setAppState,
+    isLoading,
+    fallback
+}) => {
+    setAppState(prevState => ({
+        ...prevState,
+        checkSession: !prevState.checkSession
+    }))
+
     const isAuthenticated = appState.isAuthenticated
 
     if (isLoading) return <Loading />
 
-    return isAuthenticated ? element : <Navigate to={fallback} replace />
+    return isAuthenticated ? (
+        element
+    ) : (
+        <Navigate
+            to={fallback}
+            replace
+        />
+    )
 }
 
 export default ProtectedRoute
