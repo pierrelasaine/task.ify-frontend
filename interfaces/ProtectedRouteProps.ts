61f69ba8e9cdc955c0c31f3cbd5bb8115861e5fe
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-08-07 16:10:32 -0700

    Updated Protected Route Prop Interface

--- interfaces/ProtectedRouteProps.ts
+++ interfaces/ProtectedRouteProps.ts
@@ -4,6 +4,7 @@ import AppState from './AppState'
 export default interface ProtectedRouteProps {
     element: ReactElement
     appState: AppState
+    setAppState: React.Dispatch<React.SetStateAction<AppState>>
     isLoading: boolean
     fallback: string
 }
