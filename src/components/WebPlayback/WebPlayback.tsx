d0a7ec420e95ad67af06a8b742bf3c997b35b7c5
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-08-07 15:21:33 -0700

    Resolved Merge Artifacts

--- src/components/WebPlayback/WebPlayback.tsx
+++ src/components/WebPlayback/WebPlayback.tsx
@@ -1,19 +1,19 @@
 /// <reference types="@types/spotify-web-playback-sdk"/>
-import { useState, useEffect } from "react";
-import Task from "../../../interfaces/Task"
-import AppState from "../../../interfaces/AppState"
+import { useState } from 'react'
+import Task from '../../../interfaces/Task'
+import AppState from '../../../interfaces/AppState'
 import axios from 'axios'
 
 interface WebPlaybackProps {
-  appState: AppState;
-  task: Task;
+    appState: AppState
+    task: Task
 }
 
 const WebPlayback: React.FC<WebPlaybackProps> = ({ appState, task }) => {
-    const [URI, setURI] = useState(null);
-    const playlistID = task.playlist_id;
+    const [URI, setURI] = useState(null)
+    const playlistID = task.playlist_id
 
-    (async () => {
+    ;(async () => {
         const res = await axios.get(
             `https://api.spotify.com/v1/playlists/${playlistID}`,
             {
@@ -23,24 +23,23 @@ const WebPlayback: React.FC<WebPlaybackProps> = ({ appState, task }) => {
             }
         )
 
-        setURI(res.data.uri);
+        setURI(res.data.uri)
     })()
 
+    return (
+        <>
+            <iframe
+                title='Spotify Embed: Recommendation Playlist '
+                src={`https://open.spotify.com/embed/playlist/${URI}?utm_source=generator&theme=0`}
+                width='100%'
+                height='100%'
+                style={{ minHeight: '360px' }}
+                frameBorder='0'
+                allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+                loading='lazy'
+            />
+        </>
+    )
+}
 
-  return (
-    <>
-      <iframe
-        title="Spotify Embed: Recommendation Playlist "
-        src={`https://open.spotify.com/embed/playlist/${URI}?utm_source=generator&theme=0`}
-        width="100%"
-        height="100%"
-        style={{ minHeight: "360px" }}
-        frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      />
-    </>
-  );
-};
-
-export default WebPlayback;
+export default WebPlayback
