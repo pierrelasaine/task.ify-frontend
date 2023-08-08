76c980c0452c10801d6c96786e0d68ee93754b3d
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-08-08 14:56:53 -0700

    Added Play/Pause Button

--- src/components/WebPlayback/WebPlayback.tsx
+++ src/components/WebPlayback/WebPlayback.tsx
@@ -1,11 +1,15 @@
 /// <reference types="@types/spotify-web-playback-sdk"/>
-import { useState } from 'react'
-import Task from '../../../interfaces/Task'
-import AppState from '../../../interfaces/AppState'
-import axios from 'axios'
+// import { useState } from 'react'
+// import Task from '../../../interfaces/Task'
+// import AppState from '../../../interfaces/AppState'
+// import axios from 'axios'
 import WebPlaybackProps from '../../../interfaces/WebPlaybackProps'
 
-const WebPlayback: React.FC<WebPlaybackProps> = ({ task }) => {
+const WebPlayback: React.FC<WebPlaybackProps> = ({
+    task,
+    togglePlayPause,
+    isPlaying
+}) => {
     // const [URI, setURI] = useState(null)
     const playlistID = task.playlist_id
 
@@ -25,12 +29,27 @@ const WebPlayback: React.FC<WebPlaybackProps> = ({ task }) => {
     // } catch (error: any) {
     //     console.error('Failed to get playlist URI:', error.message)
     // }
+    function playPause() {
+        togglePlayPause()
+        const iframe = document.querySelector('.spotify-embed') as HTMLIFrameElement
+        if (iframe) {
+            const iframeContent = iframe.contentWindow!
+            const playPauseButton = iframeContent.document.querySelector('.play-pause-button')! as HTMLButtonElement
+            playPauseButton.click()
+        }
+    }
 
     return (
         <>
+            <button
+                className='play-pause-button-custom'
+                onClick={() => playPause()}>
+                <h1>{isPlaying ? '||' : '>'}</h1>
+            </button>
             <iframe
                 title='Spotify Embed: Recommendation Playlist '
                 src={`https://open.spotify.com/embed/playlist/${playlistID}?utm_source=generator&theme=0`}
+                className='spotify-embed'
                 width='100%'
                 height='100%'
                 style={{ minHeight: '360px' }}
