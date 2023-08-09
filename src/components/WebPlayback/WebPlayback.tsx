25de0de570853044f4ef287314900b41539a9fef
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-08-09 10:00:32 -0700

    Eliminated Stray Brace

--- src/components/WebPlayback/WebPlayback.tsx
+++ src/components/WebPlayback/WebPlayback.tsx
@@ -9,7 +9,7 @@ const WebPlayback: React.FC<WebPlaybackProps> = ({ task }) => {
       <iframe
         className='spotify-embed'
         title="Spotify Embed: Recommendation Playlist "
-        src={`https://open.spotify.com/embed/playlist/${playlistID}}?utm_source=generator&theme=0`}
+        src={`https://open.spotify.com/embed/playlist/${playlistID}?utm_source=generator&theme=0`}
         width="100%"
         height="100%"
         frameBorder="0"
